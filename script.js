fb3d639881e0ceb31dcc174a3b5697b3086a1a2b
Joakim Silva <joakim.silva.bsc@gmail.com>
2026-09-03 05:04:32 +0100

    Add event listeners to close nav on link click

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".nav-links");
+const menuLinks = document.querySelectorAll(".nav-links a");
 
 navToggle.addEventListener("click", () => {
     navLinks.classList.toggle("nav-open");
+});
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("nav-open");
+    });
 });
